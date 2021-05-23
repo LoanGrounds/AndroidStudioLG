c1fc5aceb4a63e492159a49b35614f9b76bedc45
matibesme <83069764+matibesme@users.noreply.github.com>
2021-05-23 14:17:15 -0300

    Actualizacion 14:17 23/5

--- ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/ListaRecomendados/ListaAdaptora.java
+++ ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/ListaRecomendados/ListaAdaptora.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.ProyectoFinal.loangrounds.ListaRecomendados.Prestamo;
 import com.example.ProyectoFinal.loangrounds.R;
 
@@ -39,13 +40,45 @@ public class ListaAdaptora extends ArrayAdapter<Prestamo> {
         TextView tvNombreApellido=view.findViewById(R.id.tvNombreApellido);
         TextView precio1=view.findViewById(R.id.precio1);
         ImageView imgPrestamista=view.findViewById(R.id.imgPrestamista);
+        LottieAnimationView coraDislike=view.findViewById(R.id.coraDislike);
+        final int[] cont = {0};
 
 
         Prestamo prestamo=ListaPrestamos.get(position);
         tvNombreApellido.setText(prestamo.getName());
         precio1.setText(String.valueOf("$"+prestamo.getPrecio1()));
         imgPrestamista.setImageDrawable(mCtx.getResources().getDrawable(prestamo.getImage()));
+        final boolean[] like = {false};
+        coraDislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                like[0] =likeAnimation(coraDislike,R.raw.like, like[0]);
+
+
+                /*if (cont[0] ==0){
+                coraDislike.setBackgroundResource(R.drawable.favorite_like);
+                cont[0]++;
+                }else{
+                    coraDislike.setBackgroundResource(R.drawable.favorite_unlike);
+                    cont[0]--;
+                }*/
+            }
+        });
 
         return view;
+    }
+
+
+    private boolean likeAnimation(LottieAnimationView imageView, int animation,Boolean like){
+        if (!like){
+            imageView.setAnimation(animation);
+            imageView.playAnimation();
+        }
+        else {
+            imageView.setImageResource(R.drawable.favorite_unlike);
+        }
+
+        return !like;
     }
 }
