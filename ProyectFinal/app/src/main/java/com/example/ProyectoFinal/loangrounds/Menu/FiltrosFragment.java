88464b17e36de4010d5a430a1e67fb89a6522ef9
matibesme <83069764+matibesme@users.noreply.github.com>
2021-06-04 12:39:43 -0300

    Actualizacion 4/6 12:40

--- ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/Menu/FiltrosFragment.java
+++ ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/Menu/FiltrosFragment.java
@@ -7,13 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.example.ProyectoFinal.loangrounds.MainActivityInicio;
 import com.example.ProyectoFinal.loangrounds.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class FiltrosFragment extends Fragment {
 
     View layoutRhoot;
+    SeekBar skbMin;
+    SeekBar skbMax;
+    TextView tvPrecioMin;
+    TextView tvPrecioMax;
+    Button btnAplicarFilt;
 
     public FiltrosFragment() {
         // Required empty public constructor
@@ -33,6 +44,73 @@ public class FiltrosFragment extends Fragment {
         // Inflate the layout for this fragment
         layoutRhoot= inflater.inflate(R.layout.fragment_filtros, container, false);
 
+        ObtenerReferencia();
+        SetearListners();
+
         return layoutRhoot;
     }
+    public void SetearListners() {
+        skbMax.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                tvPrecioMax.setText("$ "+String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                skbMin.setMin(1000);
+
+                skbMax.setMax(100000);
+                skbMax.setMin(skbMin.getProgress());
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        skbMin.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                tvPrecioMin.setText("$ "+String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                skbMin.setMin(1000);
+
+                skbMax.setMax(100000);
+                skbMin.setMax(skbMax.getProgress());
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        btnAplicarFilt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivityInicio actividadContenedora;
+                actividadContenedora = (MainActivityInicio) getActivity();
+                actividadContenedora.setFragmentLupa();
+
+            }
+        });
+    }
+    private void ObtenerReferencia() {
+        skbMax = (SeekBar) layoutRhoot.findViewById(R.id.skbMax);
+        skbMin = (SeekBar) layoutRhoot.findViewById(R.id.skbMin);
+        tvPrecioMin=(TextView) layoutRhoot.findViewById(R.id.tvPrecioMin);
+        tvPrecioMax=(TextView) layoutRhoot.findViewById(R.id.tvPrecioMax);
+        btnAplicarFilt=(Button) layoutRhoot.findViewById(R.id.btnAplicarFilt);
+
+    }
+
+
 }
