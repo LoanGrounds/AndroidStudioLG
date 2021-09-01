ab485078e22ffbb41ad18e306a9d04cdcb9ed56e
matibesme <83069764+matibesme@users.noreply.github.com>
2021-09-01 08:57:20 -0300

    Actualizacion 08:57

--- ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/Menu/VerificacionPrestamoFragment.java
+++ ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/Menu/VerificacionPrestamoFragment.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ProyectoFinal.loangrounds.R;
+import com.example.ProyectoFinal.loangrounds.Utilidades.SoundPoolHelper;
 
 
 public class VerificacionPrestamoFragment extends Fragment {
+
+    View layoutRhoot;
 
 
 
@@ -31,6 +34,11 @@ public class VerificacionPrestamoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verificacion_prestamo, container, false);
+        layoutRhoot= inflater.inflate(R.layout.fragment_verificacion_prestamo, container, false);
+        int  unSonido = SoundPoolHelper.loadSound(getContext(), R.raw.sonido_correcto_prestamo);
+        SoundPoolHelper.play(unSonido);
+
+
+        return  layoutRhoot;
     }
 }
