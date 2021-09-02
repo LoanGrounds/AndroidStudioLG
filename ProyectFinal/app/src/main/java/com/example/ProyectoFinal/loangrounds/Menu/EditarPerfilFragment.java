8a9a6f0c96db969fad68e32d2e3406c503b1cebc
PatriciaBullrich <82680098+PatriciaBullrich@users.noreply.github.com>
2021-09-02 19:14:53 -0300

    agrego una tarea para obtener user por id y cambiar perfil

--- ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/Menu/EditarPerfilFragment.java
+++ ProyectFinal/app/src/main/java/com/example/ProyectoFinal/loangrounds/Menu/EditarPerfilFragment.java
@@ -8,7 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ProyectoFinal.loangrounds.AsyncTask.AsyncPostBase;
+import com.example.ProyectoFinal.loangrounds.AsyncTask.AsyncTaskGetUsuario;
+import com.example.ProyectoFinal.loangrounds.Model.Usuario;
 import com.example.ProyectoFinal.loangrounds.R;
+import com.example.ProyectoFinal.loangrounds.Utilidades.ApiHelper;
+import com.example.ProyectoFinal.loangrounds.Utilidades.Session;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 public class EditarPerfilFragment extends Fragment {
@@ -32,5 +39,31 @@ public class EditarPerfilFragment extends Fragment {
         // Inflate the layout for this fragment
         layoutRhoot=inflater.inflate(R.layout.fragment_editar_perfil, container, false);
         return layoutRhoot;
+    }
+
+
+    private class tareaEditarPerfil extends AsyncPostBase{
+
+        public tareaEditarPerfil() {
+            super(ApiHelper.devolverUrlParaGet("Usuarios", "Upadte"));
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            setParams("fechaNacimiento", "24-04-2003");
+            // Dependiendo de que cambio vas llenando params
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s!= null){
+                Gson miGson = new GsonBuilder()
+                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                        .create();
+                Session.currentUser = AsyncTaskGetUsuario.devolerUsuario(Session.currentUser.getId());
+            }
+        }
     }
 }
